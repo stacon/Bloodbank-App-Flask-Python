1b1c74bfcc2c625faab74d34fe65ecb538cbaa24
Constantinos Stathis <constantinos-@hotmail.com>
2017-11-14 16:42:02 +0200

    Small changes to get to work
    - removed unnecessary files
    - updated dependencies

--- app/router.py
+++ app/router.py
@@ -3,12 +3,12 @@ from . import config
 
 
 app = Flask(__name__)
-app.config.from_object('config')
 
 
 @app.route('/', methods=['POST', 'GET'])
 def hello_world():
-    if request.method == 'GET':
-        return render_template('index')
-    elif request.method == 'POST':
-        return render_template('index')
+    return render_template('index.html')
+    # if request.method == 'GET':
+    #     return render_template('index')
+    # elif request.method == 'POST':
+    #     return render_template('index')
